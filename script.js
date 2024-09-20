d625523575e6d99c921d1c8ed735b7e8a1324964
Yorukaa <150779385+Yorukaa@users.noreply.github.com>
2024-09-20 11:06:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const texts = [
+    // "MC SERVERS DEVELOPER",
+    "PROGRAMMER",
+    "GAMER",
+    "STREAMER",
+]
+
+const speed = 100;
+const textElements = document.querySelector(".typewriter-text")
+
+let textIndex = 0;
+let characterIndex =0;
+
+function typeWriter(){
+    if(characterIndex< texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typeWriter, speed);
+    }else{
+        setTimeout(eraseText, 1000);
+    }
+}
+
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50);
+    }else{
+        textIndex = (textIndex + 1) % texts.length;
+        characterIndex = 0;
+        setTimeout(typeWriter, 500);
+    }
+}
+
+window.onload = typeWriter;
+
+
+
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar =document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
